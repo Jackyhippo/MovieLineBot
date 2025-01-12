605287cab3f9b7c56958eaec44030a9af4872222
jacky <jackyhung0211@gmail.com>
2025-01-12 23:36:34 +0800

    20250112更新

--- commands/movie.js
+++ commands/movie.js
@@ -2,6 +2,19 @@ import axios from 'axios'
 // import * as cheerio from 'cheerio'
 import template from '../templates/movie.js'
 import fs from 'node:fs'
+import { translate } from '@vitalets/google-translate-api'
+
+// 翻譯電影標題
+async function translateText (text, targetLanguage) {
+  try {
+    const res = await translate(text, { to: targetLanguage })
+    return res.text || text // 確保返回值為字串
+  } catch (error) {
+    console.log(error)
+    console.error('Translation Error:', error)
+    return text // 出現錯誤時返回原始文字
+  }
+}
 
 export default async function movieSearch (query, event) {
   const TMDB_API_KEY = process.env.TMDB_API_KEY
@@ -9,7 +22,6 @@ export default async function movieSearch (query, event) {
 
   try {
     const response = await axios.get(tmdbUrl)
-
     // 調試用，打印完整的 API 回應
     console.log('TMDB API Response:', response.data)
 
@@ -20,28 +32,46 @@ export default async function movieSearch (query, event) {
       return
     }
 
-    // 取第一部電影資料
-    const movie = movies[0]
-    const movieTemplate = template()
-    // 回傳 movieTemplate 架構
-    console.log('Initial Movie Template:', movieTemplate)
+    // 用於存放多個 Bubble 模板
+    const templates = []
+    console.log('Templates for Carousel:', templates)
 
-    // 動態更新模板內容
-    // 填充模板內容
-    movieTemplate.body.contents[0].url = `https://image.tmdb.org/t/p/w500${movie.poster_path || ''}` // 電影海報
-    movieTemplate.body.contents[2].contents[0].contents[0].text = movie.title || '未知電影標題' // 電影標題
-    movieTemplate.body.contents[2].contents[0].contents[2].text = movie.release_date || '未提供上映日期' // 上映日期
+    // 使用 for 迴圈取到的各電影資料進模板
+    for (const movie of movies.slice(0, 10)) {
+      const movieTemplate = template()
+
+      const translatedTitle = await translateText(movie.title || '未知電影標題', 'zh-TW')
+        .catch(error => {
+          console.error('Fallback to original title due to translation error:', error)
+          return movie.title || '未知電影標題'
+        })
+
+      // 填充模板內容
+      movieTemplate.hero.url = `https://image.tmdb.org/t/p/w500${movie.poster_path || ''}` // 電影海報
+      movieTemplate.body.contents[0].text = typeof translatedTitle === 'string' ? translatedTitle : '未知電影標題' // 電影標題
+      movieTemplate.body.contents[1].contents[1].text = movie.release_date || '未提供上映日期' // 上映日期
+      movieTemplate.body.contents[2].contents[1].text = String(movie.vote_average || '未提供平均評分') // 平均評分
+      movieTemplate.body.contents[3].contents[1].text = String(movie.vote_count || '未提供評分總數') // 評分總數
+
+      // 將模板加入到陣列中
+      templates.push(movieTemplate)
+    }
 
     // 回傳 Flex Message
-    const aaa = await event.reply({
+    const result = await event.reply({
       type: 'flex',
-      altText: `${movie.title} 的電影資訊`,
-      contents: movieTemplate
+      altText: '電影搜尋結果',
+      contents: {
+        type: 'carousel',
+        contents: templates
+      }
     })
+    console.log(result)
 
-    fs.writeFileSync('./dump/movie.json', JSON.stringify(movieTemplate, null, 2))
-
-    console.log(aaa)
+    // 如果模板有錯誤則回傳到 dump 資料夾
+    if (process.env.DEBUG === 'true' && result.message) {
+      fs.writeFileSync('./dump/movie.json', JSON.stringify(templates, null, 2))
+    }
   } catch (error) {
     console.log(error)
     await event.reply('發生錯誤，請稍後再試！')
--- templates/movie.js
+++ templates/movie.js
@@ -1,75 +1,95 @@
 export default () => {
   return {
     type: 'bubble',
+    hero: {
+      type: 'image',
+      url: 'https://image.tmdb.org/t/p/w500/kl9JJ8288bNsY8oqT1SpQh1w2mb.jpg',
+      size: 'full',
+      aspectRatio: '18:20',
+      aspectMode: 'fit',
+      action: {
+        type: 'uri',
+        uri: 'https://line.me/'
+      },
+      margin: 'none',
+      position: 'relative',
+      backgroundColor: '#333333'
+    },
     body: {
       type: 'box',
       layout: 'vertical',
       contents: [
         {
-          type: 'image',
-          url: 'https://image.tmdb.org/t/p/w500/d8Ryb8AunYAuycVKDp5HpdWPKgC.jpg',
-          size: 'full',
-          aspectMode: 'cover',
-          aspectRatio: '1:1',
-          gravity: 'center'
-        },
-        {
-          type: 'box',
-          layout: 'vertical',
-          contents: [],
-          position: 'absolute',
-          background: {
-            type: 'linearGradient',
-            angle: '0deg',
-            endColor: '#00000000',
-            startColor: '#00000099'
-          },
-          width: '100%',
-          height: '40%',
-          offsetBottom: '0px',
-          offsetStart: '0px',
-          offsetEnd: '0px'
+          type: 'text',
+          text: 'The Last Airbender',
+          weight: 'bold',
+          size: 'lg',
+          style: 'italic',
+          decoration: 'underline',
+          position: 'relative',
+          align: 'start',
+          wrap: false
         },
         {
           type: 'box',
           layout: 'horizontal',
           contents: [
             {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'text',
-                  text: '音速小子3',
-                  color: '#ffffff',
-                  weight: 'bold',
-                  style: 'italic',
-                  decoration: 'underline'
-                },
-                {
-                  type: 'text',
-                  text: '上映時間',
-                  color: '#ffffff',
-                  size: 'sm'
-                },
-                {
-                  type: 'text',
-                  text: '2024-12-19',
-                  color: '#ffffff',
-                  size: 'sm'
-                }
-              ],
-              spacing: 'xs'
+              type: 'text',
+              text: '上映時間：',
+              size: 'sm',
+              flex: 0
+            },
+            {
+              type: 'text',
+              text: '2010-06-30',
+              size: 'md',
+              margin: 'xs'
             }
           ],
-          position: 'absolute',
-          offsetBottom: '0px',
-          offsetStart: '0px',
-          offsetEnd: '0px',
-          paddingAll: '20px'
+          margin: 'none'
+        },
+        {
+          type: 'box',
+          layout: 'horizontal',
+          contents: [
+            {
+              type: 'text',
+              text: '平均評分：',
+              flex: 0,
+              size: 'sm'
+            },
+            {
+              type: 'text',
+              text: '4.6',
+              size: 'md',
+              margin: 'xs'
+            }
+          ],
+          margin: 'none'
+        },
+        {
+          type: 'box',
+          layout: 'horizontal',
+          contents: [
+            {
+              type: 'text',
+              text: '評分總數：',
+              flex: 0,
+              size: 'sm'
+            },
+            {
+              type: 'text',
+              text: '3985',
+              size: 'md',
+              margin: 'xs'
+            }
+          ],
+          margin: 'none'
         }
       ],
-      paddingAll: '0px'
+      borderWidth: 'none',
+      cornerRadius: 'none'
     }
   }
 }
